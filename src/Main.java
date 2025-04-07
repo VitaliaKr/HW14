c7f39246f8dc9ded5572fee689696a4e3eb0cc4a
VitaliaKr <81364950+VitaliaKr@users.noreply.github.com>
2025-04-07 23:20:46 +0400

    done

--- src/Main.java
+++ src/Main.java
@@ -8,7 +8,11 @@ public class Main {
     }
 
     public static double avg(int num) {
-        //todo Дописать логику работы метода сюда. Метод main не трогаем!!!
-        return 0; //todo заменить 0 на корректный результат.
+        String numberString = String.valueOf(num);
+        double sum = 0;
+        for (int i = 0; i < numberString.length(); i++) {
+            sum += Integer.parseInt(String.valueOf(numberString.charAt(i)));
+        }
+        return sum / numberString.length();
     }
 }
